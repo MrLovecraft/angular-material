ddf6ff55d006f45e3d723dfb886efefb52cc46d5
MrLovecraft <cawoomer@gmail.com>
2020-12-18 18:38:48 -0600

    3 files modified, 7 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router";
 import {
   MdToolbarModule,
   MdTabsModule,
@@ -11,13 +12,33 @@ import {
   MdNativeDateModule,
   MdCheckboxModule,
   MdRadioModule
-} from '@angular/material';
+} from "@angular/material";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ExamplesComponent } from "./examples/examples.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MdToolbarModule, MdTabsModule, MdButtonModule, MdInputModule, MdDatepickerModule, MdNativeDateModule, MdCheckboxModule, MdRadioModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdTabsModule,
+    MdButtonModule,
+    MdInputModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    MdCheckboxModule,
+    MdRadioModule,
+    RouterModule.forRoot([
+      { path: "examples", component: ExamplesComponent },
+      { path: "", component: HomeComponent },
+      // otherwise redirect to home
+      { path: "**", redirectTo: "" }
+    ])
+  ],
+  declarations: [AppComponent, ExamplesComponent, HomeComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
